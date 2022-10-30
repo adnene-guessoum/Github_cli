abc6466438732c4db3264eb14723da7e9b3a685a
Adnene_wsl <adnen.guessoum@gmail.com>
2022-10-30 14:34:06 +0100

    Add Taskfile

--- github_cli/cli.py
+++ github_cli/cli.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import rich
 
+
 def get_num_stars(username: str, orgs: bool):
     """
     Parameters:
@@ -13,22 +14,24 @@ def get_num_stars(username: str, orgs: bool):
     name = username
 
     if orgs == False:
-        gh_reqs = requests.get(f'https://github.com/{name}').text
-        soup = BeautifulSoup(gh_reqs , "html.parser")
+        gh_reqs = requests.get(f"https://github.com/{name}").text
+        soup = BeautifulSoup(gh_reqs, "html.parser")
         num_stars = soup.select('a[data-tab-item="stars"] .Counter').get_text()
         return num_stars
     else:
         pass
 
+
 def count_stars(soup):
-    """ Docstring for count_stars.
+    """Docstring for count_stars.
 
     :arg1: parsed html document
     :returns: num_stars: int (number of starred repos)
 
     """
-    
+
     pass
+
 
 if __name__ == "__main__":
     pass
