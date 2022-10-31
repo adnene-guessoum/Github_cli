7f4eaeb0267464967ae180278f1b86c9d1e09154
Adnene_wsl <adnen.guessoum@gmail.com>
2022-10-31 18:56:03 +0100

    Fix linting and tests
    Add docstrings, timeout and all that was asked by linting, formating and
    testing

--- github_cli/test_cli.py
+++ github_cli/test_cli.py
@@ -1,19 +1,39 @@
-from .cli import *
+"""
+    unit test module for the functions in the cli module used to prompt a
+    search for the number of stars in a specific repo by the user of this
+    package.
+"""
+from .cli import ex_func, get_num_stars, get_org_soup
+from .cli import get_repo_number_pages
 
 
 def test_tests_success():
+    """
+    simple test to test the tests
+    """
     assert ex_func("success") == "success"
 
 
 def test_simple_user_get_num_stars():
-    test_codie = get_num_stars("codie3611", False) 
-    assert type(test_codie) is int
+    """
+    type check of return value
+    """
+    test_codie = get_num_stars("codie3611", False)
+    assert isinstance(test_codie, int)
+
 
 def test_org_get_repo_number_pages():
+    """
+    type check of return value
+    """
     test_soup = get_org_soup("facebook")
-    test_facebook = get_repo_number_pages(test_soup) 
-    assert type(test_facebook) is int
+    test_facebook = get_repo_number_pages(test_soup)
+    assert isinstance(test_facebook, int)
+
 
 def test_org_get_num_stars():
-    test_facebook = get_num_stars("facebook", True) 
-    assert type(test_facebook) is int
+    """
+    type check of return value
+    """
+    test_facebook = get_num_stars("facebook", True)
+    assert isinstance(test_facebook, int)
