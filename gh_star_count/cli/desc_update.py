e9caa188f5dfe5f5f4f3ed858c3e45ef765bceb6
Adnene_wsl <adnen.guessoum@gmail.com>
2022-11-02 13:47:34 +0100

    Add update user bio
    prompt for access token
    update by default to star, heart, tea emoji

--- gh_star_count/cli/desc_update.py
+++ gh_star_count/cli/desc_update.py
@@ -4,13 +4,35 @@ Module allowing user description update on his github account
 import os
 from github import Github
 
-def get_current_user_auth():
-    "
+def get_user_infos():
+    """
+    Prompt the user to give login info 
+    """
+    token = input("Github account access token (if you don't have one please visit https://github.com/settings/tokens) : ")
+
+    return token
+ 
+
+def get_current_user_auth(login_info: str):
+    """
     get current user object for github profile update
-    "
-    token = os.getenv('GITHUB_TOKEN')
-    g = Github(token)
-    return g
+    """
+    auth_user_obj = Github(login_info).get_user()
+    return auth_user_obj
+
+def update_user_bio(auth_user_obj: object):
+    """
+    update user bio with star, heart and tea emoji 
+    """
+    default =  ":star: :gift_heart: :tea:"
+    print(f"default bio setting : {default}")
+    new_str = input("New bio for your account : ") or ":star: :gift_heart: :tea:"
+    auth_user_obj.edit(bio = new_str)
+
+
+def update_user_status():
 
 if __name__ == "__main__":
-
+    login = get_user_infos() 
+    authorized_user = get_current_user_auth(login) 
+    update_user_bio(authorized_user)
--- gh_star_count/cli/test_desc_update.py
+++ gh_star_count/cli/test_desc_update.py
@@ -1,9 +1,12 @@
 """
 Unit test for the description update functions of the command line interface
 """
-from desc_update import get_current_user_auth
+from .desc_update import get_current_user_auth
 
 
 def test_get_current_user_auth():
-    g = get_current_user_auth()
-    assert g.get_user() = u'adnene-guessoum'
+    """
+    Unittest fetching user auth token in env
+    """
+    g_object = get_current_user_auth()
+    assert g_object.get_user() == None
